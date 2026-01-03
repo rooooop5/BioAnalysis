716c1f8dc7319819dab67acd4c051c4057600d0e
Saswata Mondal <mondalsaswata2005@gmail.com>
2026-01-03 13:44:25 +0530

    commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,Request,Response
+from fastapi.responses import JSONResponse
 from app.router.dna_routes import dna_router
 app=FastAPI()
-app.include_router(dna_router)
 
+
+@app.exception_handler(HTTPException)
+def handler(exception:HTTPException,req:Request):
+    return JSONResponse(status_code=exception.status_code,
+                        content=
+                            {"error":
+                                 {"status_code":exception.status_code,
+                                  "message":exception.detail,"path":req.url.path
+                                 }
+                            }
+                        )
+
+app.include_router(dna_router)
--- app/router/dna_routes.py
+++ app/router/dna_routes.py
@@ -1,8 +1,19 @@
+from typing import TYPE_CHECKING
 from fastapi import APIRouter,Query
 from app.schemas.dna_schemas import DNASequence,DNAAnalysisOptions,DNAAnalysisResponse
+
+if TYPE_CHECKING:
+    def analyze_dna(dna:DNASequence):
+        pass
+    def dna_validity(dna:DNASequence):
+        pass
 
 dna_router=APIRouter(prefix="/dna",tags=["DNA"])
 
 @dna_router.post("/analyze",response_model=DNAAnalysisResponse,summary="DNA Sequence Analysis and Validity Check")
-def analysis(req:DNASequence,options:DNAAnalysisOptions=Query()):
-    return {"msg":"ok"}
+def analysis(dna:DNASequence,options:DNAAnalysisOptions=Query()):
+    return analyze_dna(dna)
+
+@dna_router.post("/check-validity",response_model=DNAAnalysisResponse,response_model_include={"validity"})
+def check_validity(dna:DNASequence):
+    return dna_validity(dna)
