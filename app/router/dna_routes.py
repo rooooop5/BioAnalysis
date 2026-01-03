e51eddb6c5265240df9c2387cf1cf5fae32ac604
Saswata Mondal <mondalsaswata2005@gmail.com>
2026-01-03 16:13:26 +0530

    Formatted code

--- app/router/dna_routes.py
+++ app/router/dna_routes.py
@@ -1,21 +1,31 @@
 from typing import TYPE_CHECKING
-from fastapi import APIRouter,Query
-from app.schemas.dna_schemas import DNASequence,DNAAnalysisOptions,DNAAnalysisResponse
+
+from fastapi import APIRouter, Query
+
+from app.schemas.dna_schemas import DNAAnalysisOptions, DNAAnalysisResponse, DNASequence
 
 if TYPE_CHECKING:
-    def analyze_dna(dna:DNASequence,options:DNAAnalysisOptions):
+
+    def analyze_dna(dna: DNASequence, options: DNAAnalysisOptions):
         pass
-    def dna_validity(dna:DNASequence):
+
+    def dna_validity(dna: DNASequence):
         pass
 
-dna_router=APIRouter(prefix="/dna",tags=["DNA"])
+
+dna_router = APIRouter(prefix="/dna", tags=["DNA"])
 
 
-@dna_router.post("/check-validity",response_model=DNAAnalysisResponse,response_model_include={"validity"},summary="DNA Sequence Validity Check")
-def check_validity(dna:DNASequence):
+@dna_router.post(
+    "/check-validity",
+    response_model=DNAAnalysisResponse,
+    response_model_include={"validity"},
+    summary="DNA Sequence Validity Check",
+)
+def check_validity(dna: DNASequence):
     return dna_validity(dna)
 
-@dna_router.post("/analyze",response_model=DNAAnalysisResponse,summary="DNA Sequence Analysis")
-def analysis(dna:DNASequence,options:DNAAnalysisOptions=Query()):
-    return analyze_dna(dna,options)
 
+@dna_router.post("/analyze", response_model=DNAAnalysisResponse, summary="DNA Sequence Analysis")
+def analysis(dna: DNASequence, options: DNAAnalysisOptions = Query()):
+    return analyze_dna(dna, options)
