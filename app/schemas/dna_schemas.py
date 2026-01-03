d4883d523a8711fa524421abf34b5bb08407bbf2
Saswata Mondal <mondalsaswata2005@gmail.com>
2026-01-03 09:31:33 +0530

    Something happened this the dna schema file got deleted.

---
+++ app/schemas/dna_schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel,Field
+
+#----model of the dna seq request-----
+class DNASequence(BaseModel):
+    seq:str
+
+
+#-----model for the query params-------
+class DNAAnalysisOptions(BaseModel):
+    gc_content:bool
+    nucleotide_count:bool
+    reverse_compliment:bool
+    check_validity:bool
+
+
+class DNAAnalysisResponse(BaseModel):
+    gc_content:float|None
+    nucleotide_count:dict|None
+    reverse_compliment:str|None
+    validity:bool|None
+
+#-----function signature for Shubh: def analyze_dna(seq:DNASequence,options:DNAAnalysisOptions)->DNAAnalysisResponse
+#----- reponse model not designed yet------
