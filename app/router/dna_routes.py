f3b72651ab8784d6eb6d6152a7f0910f54901356
Saswata Mondal <mondalsaswata2005@gmail.com>
2026-01-03 14:15:18 +0530

    Changed dna router

--- app/router/dna_routes.py
+++ app/router/dna_routes.py
@@ -3,17 +3,19 @@ from fastapi import APIRouter,Query
 from app.schemas.dna_schemas import DNASequence,DNAAnalysisOptions,DNAAnalysisResponse
 
 if TYPE_CHECKING:
-    def analyze_dna(dna:DNASequence):
+    def analyze_dna(dna:DNASequence,options:DNAAnalysisOptions):
         pass
     def dna_validity(dna:DNASequence):
         pass
 
 dna_router=APIRouter(prefix="/dna",tags=["DNA"])
 
-@dna_router.post("/analyze",response_model=DNAAnalysisResponse,summary="DNA Sequence Analysis and Validity Check")
-def analysis(dna:DNASequence,options:DNAAnalysisOptions=Query()):
-    return analyze_dna(dna)
 
-@dna_router.post("/check-validity",response_model=DNAAnalysisResponse,response_model_include={"validity"})
+@dna_router.post("/check-validity",response_model=DNAAnalysisResponse,response_model_include={"validity"},summary="DNA Sequence Validity Check")
 def check_validity(dna:DNASequence):
     return dna_validity(dna)
+
+@dna_router.post("/analyze",response_model=DNAAnalysisResponse,summary="DNA Sequence Analysis")
+def analysis(dna:DNASequence,options:DNAAnalysisOptions=Query()):
+    return analyze_dna(dna,options)
+
